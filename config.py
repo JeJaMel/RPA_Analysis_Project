92928b75552fd051c4eadf343af99dc7563e4209
JeJaMel <jjmelendezb15@gmail.com>
2025-03-05 22:17:07 -0400

    feat: Add masked twilio config

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+ACCOUNT_SID = "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  # Tu Account SID de Twilio
+AUTH_TOKEN  = "your_auth_token"  # Tu Auth Token de Twilio
+TWILIO_PHONE_NUMBER = "+1xxxxxxxxxx"  # Tu número de teléfono de Twilio
+DESTINATION_PHONE_NUMBER = "+1xxxxxxxxxx"  # El número de WhatsApp al que enviarás el reporte (debe estar verificado)
